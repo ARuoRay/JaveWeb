e1a7318014c4e35651148eae20e1f75e5c98e347
ARuoRay <ray861022@gmail.com>
2024-11-06 14:19:54 +0800

    1106

--- springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
@@ -10,10 +11,12 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.bean.Book;
 import com.example.demo.response.ApiResponse;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -102,6 +105,49 @@ public class ApiController {
 	 * 提示: IntSummaryStatistics, Collectors.partitioningBy
 	 * */
 	
+	
+	
+	/**
+	 * 7. 多筆參數轉 Map
+	 * name 書名(String), price 價格(Double), amount 數量(Integer), pub 出刊/停刊(Boolean)
+	 * 路徑: /book?name=Math&price=12.5&amount=10&pub=true
+	 * 路徑: /book?name=English&price=10.5&amount=20&pub=false
+	 * 網址: http://localhost:8080/api/book?name=Math&price=12.5&amount=10&pub=true
+	 * 網址: http://localhost:8080/api/book?name=English&price=10.5&amount=20&pub=false
+	 * 自動會轉為 Map 集合
+	 * */
+	
+	
+	
+	/**
+	 * 8. 多筆參數轉指定 Bean 物件
+	 */
+	
+	
+	
+	/**
+	 * 9. 路徑參數
+	 * 路徑: /book/1
+	 * 路徑: /book/3
+	 * 網址: http://localhost:8080/api/book/1
+	 * 網址: http://localhost:8080/api/book/3
+	 */
+	@GetMapping("/book/{id}")
+	public ResponseEntity<ApiResponse<Book>>getBookById(@PathVariable Integer id) {
+		List<Book> books=List.of(
+				new Book(1, "Math1", 12.5, 20, true),
+				new Book(2, "Math2", 13.5, 21, false),
+				new Book(3, "Math3", 14.5, 22, true),
+				new Book(4, "Math4", 15.5, 23, false),
+				new Book(5, "Math5", 16.5, 24, true)
+				);
+		Optional<Book>optBook=books.stream().filter(b->b.getId().equals(id)).findAny();
+		
+		if(optBook.isEmpty()) {
+			throw new RuntimeException("查無此書");
+		}
+		return ResponseEntity.ok(ApiResponse.success("查詢成功",optBook.get()));
+	}
 	
 	
 }
